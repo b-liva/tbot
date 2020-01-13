79ae4062c56841ad5278992650ea7cb66185f89d
name <ebammo20@gmail.com>
2020-01-13 22:23:27 +0330

    modifying bot token key

--- mtph.py
+++ mtph.py
@@ -16,7 +16,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-token = os.environ['TOKEN']
+token = os.environ['INFORMMTPH_TOKEN']
 
 
 def start(update, context):
